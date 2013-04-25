434a77bc85e473f471e43a4dfa9d41843fa02270
Sergey7br <gd_mops@mail.ru>
2013-04-25 16:08:48 +0400

    9 частей

--- itsbeta_iphone/Resources/Views/preViewAchivs.js
+++ itsbeta_iphone/Resources/Views/preViewAchivs.js
@@ -33,71 +33,180 @@ module.exports = [
 				}
 			},
 			{
-				outlet: "bonus",
 				style: {
 					className: "Ti.UI.View",
-					layout: "vertical",
 					top: 90,
 					bottom: 5,
 					height: Ti.UI.SIZE,
 					width: "90%",
-					backgroundImage: "images/bg/Panel.png",
 					backgroundLeftCap: 13,
 					backgroundTopCap: 13
 				},
 				subviews:
 				[
 					{
-						outlet: "closeClick",
+						style: {
+							className: "Ti.UI.View",
+							top: 0,
+							left: 0,
+							height: 16,
+							width: 16,
+							backgroundImage: "images/paper/Paper_background_topLeft.png"
+						}
+					},
+					{
+						style: {
+							className: "Ti.UI.View",
+							top: 0,
+							left: 16,
+							right: 16,
+							height: 16,
+							backgroundImage: "images/paper/Paper_background_topMiddle.png"
+						}
+					},
+					{
 						style: {
 							className: "Ti.UI.View",
 							top: 0,
 							right: 0,
-							height: 40,
-							width: 40,
-							backgroundColor: "transparent"
+							height: 16,
+							width: 16,
+							backgroundImage: "images/paper/Paper_background_topRight.png"
+						}
+					},
+					{
+						style: {
+							className: "Ti.UI.View",
+							top: 16,
+							left: 0,
+							width: 16,
+							height: "93%",
+							//bottom: 16,
+							backgroundImage: "images/paper/Paper_background_middleLeft.png"
+						}
+					},
+					{
+						style: {
+							className: "Ti.UI.View",
+							top: 16,
+							left: 16,
+							right: 16,
+							height: "93%",
+							//bottom: 16,
+							backgroundImage: "images/paper/Paper_background_middleMiddle.png"
+						}
+					},
+					{
+						style: {
+							className: "Ti.UI.View",
+							top: 16,
+							right: 0,
+							width: 16,
+							height: "93%",
+							//bottom: 16,
+							backgroundImage: "images/paper/Paper_background_middleRight.png"
+						}
+					},
+					{
+						style: {
+							className: "Ti.UI.View",
+							top: "96%",
+							left: 0,
+							width: 16,
+							height: 16,
+							//bottom: 16,
+							backgroundImage: "images/paper/Paper_background_bottomLeft.png"
+						}
+					},
+					{
+						style: {
+							className: "Ti.UI.View",
+							top: "96%",
+							left: 16,
+							right: 16,
+							height: 16,
+							//bottom: 16,
+							backgroundImage: "images/paper/Paper_background_bottomMiddle.png"
+						}
+					},
+					{
+						style: {
+							className: "Ti.UI.View",
+							top: "96%",
+							right: 0,
+							height: 16,
+							width: 16,
+							backgroundImage: "images/paper/Paper_background_bottomRight.png"
+						}
+					},
+					{
+						outlet: "bonus",
+						style: {
+							className: "Ti.UI.View",
+							layout: "vertical",
+							top: 0,
+							//bottom: 5,
+							height: Ti.UI.SIZE,
+							width: Ti.UI.FILL,
+							//backgroundImage: "images/bg/Panel.png",
+							//backgroundLeftCap: 13,
+							//backgroundTopCap: 13
 						},
 						subviews:
 						[
 							{
-								outlet: "close",
+								outlet: "closeClick",
 								style: {
 									className: "Ti.UI.View",
-									top: 5,
-									right: 5,
-									height: 22,
-									width: 22,
-									backgroundImage: "images/buttons/Close.Normal.png"
+									top: 0,
+									right: 0,
+									height: 40,
+									width: 40,
+									backgroundColor: "transparent"
+								},
+								subviews:
+								[
+									{
+										outlet: "close",
+										style: {
+											className: "Ti.UI.View",
+											top: 5,
+											right: 5,
+											height: 22,
+											width: 22,
+											backgroundImage: "images/buttons/Close.Normal.png"
+										}
+									}
+								]
+							},
+							{
+								outlet: "nameAchivs",
+								style: {
+									className: "Ti.UI.Label",
+									top: 30,
+									left: 20,
+									height: Ti.UI.SIZE,
+									width: 220,
+									text: "Крутая ачивка",
+									color: "#777575",
+									font: {fontSize: 21, fontWeight: "regular",fontFamily: "Helvetica"}
+									
+								}
+							},
+							{
+								outlet: "textAchivs",
+								style: {
+									className: "Ti.UI.View",
+									top: 10,
+									left: 15,
+									bottom: 0,
+									backgroundColor: "transparent",
+									height: Ti.UI.SIZE,
+									width: 220,
 								}
 							}
 						]
 					},
-					{
-						outlet: "nameAchivs",
-						style: {
-							className: "Ti.UI.Label",
-							top: 30,
-							left: 20,
-							height: Ti.UI.SIZE,
-							width: 220,
-							text: "Крутая ачивка",
-							color: "#777575",
-							font: {fontSize: 21, fontWeight: "regular",fontFamily: "Helvetica"}
-							
-						}
-					},
-					{
-						outlet: "textAchivs",
-						style: {
-							className: "Ti.UI.View",
-							top: 10,
-							left: 15,
-							bottom: 0,
-							backgroundColor: "transparent",
-							height: Ti.UI.SIZE,
-							width: 220,
-						}
-					}
 				]
 			},
 			{
